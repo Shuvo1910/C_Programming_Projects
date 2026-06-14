9a009513e4a18455f726c28163a0a5115962bd73
Sultan <211139503+Shuvo1910@users.noreply.github.com>
2026-06-14 21:42:51 +0600

    Add files via upload

---
+++ CUBE_Volume.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main () {
+
+     int h, w,d;
+
+     h = 10;
+     w = 12;
+     d = 8;
+
+     int volume = h*w *d;
+
+     printf("Volume of CUBE is: %d\n", volume);
+
+return 0;
+}
